cc39bc19631c36c73ff734ec7b4d5ef6ab217e03
eulampio <alesantoseulampio@gmail.com>
2025-05-23 17:05:47 -0400

    ainda falta o imput cobrir os icon
    feitos os inp mas ainta falta detalhes como a linha cobrir os icom

--- HomePgPrincipalP4/main.js
+++ HomePgPrincipalP4/main.js
@@ -21,3 +21,31 @@
       container.insertAdjacentHTML("beforeend", criarCampoPergunta(contador));
       contador++;
     }
+
+      <div id="container-perguntas"></div>
+  <button onclick="adicionarPergunta()">Adicionar Pergunta</button>
+
+  <script>
+    const criarCampoPergunta = (indice) => {
+      return `
+        <form action="#" method="post"> 
+          <label for="nome_completo_${indice}">${indice}-</label>
+          <input class="edita_perguntad_adm" type="text" name="nome_completo_${indice}" id="nome_completo_${indice}" required> 
+          <button type="button" class="img_edite_pergunta">
+            <img src="edit_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg" alt="Editar">
+          </button>
+          <button type="button">
+            <img src="delete_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg" alt="Excluir">
+          </button>
+        </form>
+      `;
+    };
+
+    let contador = 1;
+
+    function adicionarPergunta() {
+      const container = document.getElementById("container-perguntas");
+      container.insertAdjacentHTML("beforeend", criarCampoPergunta(contador));
+      contador++;
+    }
+  </script>
