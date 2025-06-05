f367319ede3d2e6ce0272e64e2d94df43a8d0332
eulampio <alesantoseulampio@gmail.com>
2025-06-05 14:10:28 -0400

    as
    sa

--- HomePgPrincipalP4/main.js
+++ HomePgPrincipalP4/main.js
@@ -1,51 +1,11 @@
+let rosa = 8;
+let lirio = 10;
+let tulipa = 2;
 
-    const criarCampoPergunta = (indice) => {
-      return `
-        <form action="#" method="post"> 
-          <label for="nome_completo_${indice}">${indice}-</label>
-          <input class="edita_perguntad_adm" type="text" name="nome_completo_${indice}" id="nome_completo_${indice}" required> 
-          <button type="button" class="img_edite_pergunta">
-            <img src="edit_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg" alt="Editar">
-          </button>
-          <button type="button">
-            <img src="delete_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg" alt="Excluir">
-          </button>
-        </form>
-      `;
-    };
+let  quantidadeRosa = 70;
+let  quantidadeLirio = 50;
+let  quantidadeTulipa = 120;
 
-    let contador = 1;
-
-    function adicionarPergunta() {
-      const container = document.getElementById("container-perguntas");
-      container.insertAdjacentHTML("beforeend", criarCampoPergunta(contador));
-      contador++;
-    }
-
-      <div id="container-perguntas"></div>
-  <button onclick="adicionarPergunta()">Adicionar Pergunta</button>
-
-  <script>
-    const criarCampoPergunta = (indice) => {
-      return `
-        <form action="#" method="post"> 
-          <label for="nome_completo_${indice}">${indice}-</label>
-          <input class="edita_perguntad_adm" type="text" name="nome_completo_${indice}" id="nome_completo_${indice}" required> 
-          <button type="button" class="img_edite_pergunta">
-            <img src="edit_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg" alt="Editar">
-          </button>
-          <button type="button">
-            <img src="delete_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg" alt="Excluir">
-          </button>
-        </form>
-      `;
-    };
-
-    let contador = 1;
-
-    function adicionarPergunta() {
-      const container = document.getElementById("container-perguntas");
-      container.insertAdjacentHTML("beforeend", criarCampoPergunta(contador));
-      contador++;
-    }
-  </script>
+let 
+let 
+let 
